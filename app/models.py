62db8cc48fe16cbeaf6b1ff3c8ed2173ac524d00
zhangweijie <zhangweijie@wtsd.cn>
2019-01-04 16:45:35 +0800

    add relationship

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    post = db.relationship('Post', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.nickname)
@@ -14,7 +15,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
-    user = db.Column(db.String(64), db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
