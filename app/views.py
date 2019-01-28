f125e0a84bf5e5d4633797f78968d5b76cefd846
zhangweijie <zhangweijie@wtsd.cn>
2019-01-28 13:23:07 +0800

    Make Restful API

--- app/views.py
+++ app/views.py
@@ -1,8 +1,23 @@
 from app import app, db, lm, oid
-from flask import render_template, flash, redirect, session, url_for, request, g
+from flask import render_template, flash, redirect, session, url_for, request, g, jsonify, abort, make_response
 from flask_login import login_user, logout_user, current_user, login_required
 from .forms import LoginForm
 from .models import User
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
 
 
 @app.route('/')
@@ -92,3 +107,35 @@ def user(nickname):
 @login_required
 def show_chart():
     return render_template('render.html')
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': str(error)}), 404)
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if not task:
+        abort(404)
+    return jsonify({'tasks': task[0]})
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or 'title' not in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
